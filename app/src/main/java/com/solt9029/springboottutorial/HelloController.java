113994ac593827155e783e01f63121db85558d80
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-04-17 22:39:09 +0900

    docker

---
+++ app/src/main/java/com/solt9029/springboottutorial/HelloController.java
@@ -0,0 +1,12 @@
+package com.solt9029.springboottutorial;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
