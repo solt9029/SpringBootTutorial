98b5393d603d234b04aec4420b87d356dd01b567
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-04-18 21:44:40 +0900

    docker working

--- app/src/main/java/com/solt9029/springboottutorial/HelloController.java
+++ app/src/main/java/com/solt9029/springboottutorial/HelloController.java
@@ -1,12 +1,12 @@
 package com.solt9029.springboottutorial;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello!";
     }
 }
